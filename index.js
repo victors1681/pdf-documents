177ce44c74b4bd51267b8123c8f3abb061d82a27
victors1681 <v.santos@itsoluclick.com>
2022-11-09 17:13:51 -0500

    exporting receipt

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 const { createDocument } = require("./createDocument.js");
-
+const { createReceipt } = require("./createReceipt")
 exports.createDocument = createDocument;
+exports.createReceipt = createReceipt;
